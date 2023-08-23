a199c9595df519c1b842d1d7297f0e87f5326a17
nicole mcinerney <nicolemci@hotmail.com>
2023-08-23 11:59:17 +0100

    new and show in controller and views

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,27 @@ class FlatsController < ApplicationController
   def home
     @flats = Flat.all
   end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:address, :price, :capacity)
+  end
 end
