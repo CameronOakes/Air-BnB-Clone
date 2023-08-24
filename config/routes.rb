759e0675a003e868cdf27b108f353976dacd1391
nicole mcinerney <nicolemci@hotmail.com>
2023-08-24 13:46:06 +0100

    backend for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "flats#home"
-  resources :flats, only: [:new, :create, :show]
+
+  resources :flats, only: [:new, :create, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index]
 end
